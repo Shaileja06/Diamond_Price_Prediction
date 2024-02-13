8d4acfde96ec6184c60fc1f460508ec159ccd258
Shaileja06 <shailejapatil5@gmail.com>
2024-02-13 21:53:33 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
+
 def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open (file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n", "")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
         
         return requirements
    
@@ -16,5 +21,4 @@ setup(
     author_email='shailejapatil5@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
-    
 )    
